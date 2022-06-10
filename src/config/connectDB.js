ae906254e5c4d0d745473b3dffc29e2aac771fdc
vinhphan812 <vinhphan812@gmail.com>
2022-06-11 00:43:24 +0700

    sync code

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,27 +1,18 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
+const sequelize = new Sequelize("CarRental", "car_admin", "Qq123456789", {
+	host: "139.59.104.129",
+	port: 3306,
+	dialect: "mysql",
+	logging: false,
+});
 
-
-// Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('d9abdq6ecp1u3k', 'gvtwrizdcjgtun', '2f8b0c3118c39a00d5cbbb397f41643bb9c7963a7f79310623718220bf7be750', {
-  host: 'ec2-54-165-184-219.compute-1.amazonaws.com',
-  dialect: 'postgres',
-  logging:false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  }
-
-}); 
-
-let connectDB= async()=>{
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-      } catch (error) {
-        console.error('Unable to connect to the database:', error);
-      }
-}
+let connectDB = async () => {
+	try {
+		await sequelize.authenticate();
+		console.log("Connection has been established successfully.");
+	} catch (error) {
+		console.error("Unable to connect to the database:", error);
+	}
+};
 module.exports = connectDB;
